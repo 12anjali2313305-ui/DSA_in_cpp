1fb11a18a1325f7ee25513eac87a1b866a60d3d6
Anjali singh <12anjali2313305@gmail.com>
2026-06-21 21:37:26 +0530

    Create leetcode448.cpp

---
+++ leetcode/leetcode448.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+         int n=nums.size();
+         vector<int> v;
+         
+         for(int i=1;i<=n;i++)
+         {
+              bool flag =false;
+              for(int j=0;j<n;j++)
+              {
+                if(nums[j]==i)
+                {
+                    flag=true;
+                    break;
+                }
+              }
+              if(flag==false)
+              {
+                v.push_back(i);
+              }
+         }
+         return v;
+         
+
+         
+    }
+};
