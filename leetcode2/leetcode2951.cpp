d199ef86fb2a9fba7fbad3a425de301303df2c51
Anjali singh <12anjali2313305@gmail.com>
2026-07-09 19:04:39 +0530

    Create leetcode2951.cpp

---
+++ leetcode2/leetcode2951.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& arr) {
+        int n=arr.size();
+        int j=0;
+       vector<int> ans;
+       for(int i=1;i<n-1;i++)
+       {
+        if(arr[i]>arr[i+1] && arr[i]>arr[i-1])   
+        {
+            ans.push_back(i);
+        }
+       }
+       return ans;
+    }
+};
