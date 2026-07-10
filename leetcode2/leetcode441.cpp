abb61ab9e85dddd4700df2edcccb12f941912c09
Anjali singh <12anjali2313305@gmail.com>
2026-07-10 22:04:17 +0530

    Implement arrangeCoins function for coin arrangement

---
+++ leetcode2/leetcode441.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long low=1;
+        long long high=n;
+        while(low<=high)
+        {
+            long long k=low+(high-low)/2;
+            long long m=k*(k+1)/2;
+            if(m==n) return (int)k;
+            if(m>n) high=k-1;
+            else low=k+1;
+        }
+        return (int)high;
+    }
+};
