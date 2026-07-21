740360c685d982b02e13bc780e7cac99d8aa8e31
Anjali singh <12anjali2313305@gmail.com>
2026-07-21 23:27:55 +0530

    Implement intersection function for two arrays

---
+++ leetcode2/leetcode349.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
+        int m=nums2.size();
+        sort(nums1.begin(),nums1.end());
+         sort(nums2.begin(),nums2.end());
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {   if(i>0 && nums1[i]==nums1[i-1])   
+                continue;
+            for(int j=0;j<m;j++)
+            {
+                if(nums1[i]==nums2[j])
+                {
+                   ans.push_back(nums1[i]);
+                   nums2[j]=-1;
+                   break;
+                }
+            }
+
+        }
+        return ans;
+        
+    }
+};
